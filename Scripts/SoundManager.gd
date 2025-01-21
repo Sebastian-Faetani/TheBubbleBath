89aef55de03c7c8b160fcbee113eb8523739024a
Alain Gerfauo <memofrezzo@gmail.com>
2025-01-21 20:55:18 -0300

    error menu

--- Scripts/SoundManager.gd
+++ Scripts/SoundManager.gd
@@ -1,1 +1,5 @@
 extends Node
+var music: bool
+@onready var button_click: AudioStreamPlayer = $ButtonClick
+func ButtonClick ():
+	button_click.play()
---
+++ Scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	SoundManager.ButtonClick()
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
