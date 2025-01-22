c2d24ca19fac141aa62b79a55df4c7f394697bff
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2025-01-22 19:04:25 -0300

    Moving Candle

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -4,10 +4,13 @@ var BoggartOn : bool
 var WindowOpened : bool
 var WindowOpening : bool
 var WindowClosing : bool
-
+var DoorOpening : bool
+var DoorOpen : bool
 
 func _ready() -> void:
 	BoggartOn = false
 	WindowOpened = false
 	WindowOpening = false
 	WindowClosing = false
+	DoorOpening = false
+	DoorOpen = false
