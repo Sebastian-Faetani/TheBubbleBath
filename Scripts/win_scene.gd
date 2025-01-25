5a6c9c6281b2cb1a734abc4eff13d1aa44776f3b
Alain Gerfauo <memofrezzo@gmail.com>
2025-01-25 17:02:23 -0300

    escenas agregadas

--- Scripts/win_scene.gd
+++ Scripts/win_scene.gd
@@ -1,5 +1,17 @@
 extends Control
 
-
+func _ready() -> void:
+    MusicManager.fondoMenuStart()
+    
 func _on_menu_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+    MusicManager.fondoMenuStop()
+    get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+func _on_exit_pressed() -> void:
+    MusicManager.fondoMenuStop()
+    SoundManager.ButtonClicked()
+    get_tree().quit()
+
+
+func _on_retry_pressed() -> void:
+    MusicManager.fondoMenuStop()
+    get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
